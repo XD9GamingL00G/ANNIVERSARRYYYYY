b76bc9c050985d22b00c26755882b5ca9b5f25c4
Devraaj Jani <XD9GamingL00G@users.noreply.github.com>
2026-05-04 16:55:59 -0700

    Remove Share the Love button and keep only Close button on result page

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-const shareButton = document.getElementById("shareButton");
 const triviaButton = document.getElementById("triviaButton");
 const secretButton = document.getElementById("secretButton");
-
-shareButton.addEventListener("click", () => {
-  window.open("share.html", "_blank");
-});
 
 triviaButton.addEventListener("click", () => {
   window.open("trivia.html", "_blank");
